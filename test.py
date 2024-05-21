75065e7bfd1059505f40a89d9a34e4c5ab21906f
tengfei666680 <83683292+tengfei666680@users.noreply.github.com>
2024-05-21 14:31:06 +0800

    Update test.py

--- test.py
+++ test.py
@@ -290,7 +290,7 @@ def test(data,
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(prog='test.py')
     parser.add_argument('--weights', nargs='+', type=str, default='yolov7.pt', help='model.pt path(s)')
-    parser.add_argument('--data', type=str, default='/data/yolov7-main/data/aircraft.yaml', help='*.data path')
+    parser.add_argument('--data', type=str, default='', help='*.data path')
     parser.add_argument('--batch-size', type=int, default=64, help='size of each image batch')
     parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
     parser.add_argument('--conf-thres', type=float, default=0.001, help='object confidence threshold')
